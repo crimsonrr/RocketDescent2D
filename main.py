bbc931c6c176d8424086cd7cd8b1b7c4751afa55
crimsonrr <ironspiderp8@gmail.com>
2026-05-22 00:12:45 -0400

    refined the computer logic, clamped many variables to reduce errors, refined the gimbal tilting mechanism by making it only control rotation movement and theta control the direction of the thrust (forcex and forcey). still need to work on a better graph output, which can be achieved in modeling a "suicide burn" situation model in the computer instead of gradually slowing down the vehicle.

--- main.py
+++ main.py
@@ -19,14 +19,11 @@ l_thrust = 2.0 # distance from the engine nozzle to the rocket's COM  (m)
 # proportionality constant determines how aggressively the rocket should tilt in the opposite direction
 tilt_limit = 0.40 # ~23 degrees
 kp = 0.3 # Proportional controller: how much the gimbal should react when a tilt is noticed
-kd = 0.15 # derivative of theta, looks at how fast the rocket is spinning and applies a counter force to slow the spinning down
-
-engine_ignited = False
+kd = 0.15 # modifies the velocity (d/dx), looks at how fast the rocket is spinning and applies a counter force to slow the spinning down
 
 mass = 1.0
 g = 9.81
 I = 0.05
-
 # log both dimensions to plot the rocket's flight path
 x_history = []
 y_history = []
@@ -35,48 +32,55 @@ y_history = []
 print("Starting Simulation...")
 
 while y > 0.0:
-    altitude_weight = np.clip(y / 100.0, 0.0, 1.0) # with this variable, the lower to the ground the rocket is, the less horizontal tilt adjustments are made
-    target_theta = np.clip(-0.25 * v_x * altitude_weight, -tilt_limit, tilt_limit)  # forces the rocket to tilt in the opposite direction and fight the initial rightwards horizontal movement (v_x0 positive), if the rocket tilts too far to the left and ends up completely changing directions, the negatives cancel, and it will redirect itself.
-    target_theta += -0.1 * theta
-    error = theta - target_theta
-    gimbal_angle = -(kp * error + kd * omega)
-    gimbal_angle= np.clip(gimbal_angle, -0.26, 0.26, out=None)
-
-# via kinematics, where the final velocity must be equal to zero
-    accel_needed = -(v_y ** 2) / (2 * y)
-# via f=ma
     max_thrust = 30.0
     accel_max = (max_thrust - (mass * g)) / mass
+    v_safe = np.sqrt(2 * accel_max * max(y, 0))
+    flare_altitude = 30.0  # at this point, the rocket must increase throttle as it is nearing the ground
+    if y > flare_altitude:
+        k = 0.5
+        desired_vx = -k * x  # direct the velocity to the opposite direction
+        desired_vy = -0.3 * v_safe  # stay within stopping capability
+        desired_vy = max(desired_vy, -v_safe)
+        if v_y < desired_vy:
+            desired_vy = -v_safe * 0.8 # allows the rocket "catch up" and actually achieve the v_safe velocity
 
-# when this occurs, the rocket has reached the absolute last second it has to break, therefore ignition begins
-    if v_y < 0: # multiply by safety factor to account for some thrust lost trying to align itself horizontally
-       effective_accel = accel_max * np.cos(theta + gimbal_angle)
-       stopping_distance = (v_y ** 2) / (2 * effective_accel) # kinematics
-    else:
-        stopping_distance = 0
-    if y <= stopping_distance * 2.0:
-        engine_ignited = True
-# dynamically throttle the engine
-    if engine_ignited:
-        target_theta *= 0.2
-
-        k_v = 4.0
-        k_p = 2.0
-        desired_vy = -1.0 # soft landing desired
+        vx_error = desired_vx - v_x
         vy_error = desired_vy - v_y
-
-        thrust = mass * (g + k_v * vy_error)
-        thrust = np.clip(thrust, 0.0, max_thrust)
+        ky = 1.5
+        kx = 0.5
+        ay_desired = ky * vy_error
+        ay_desired = np.clip(ay_desired, -accel_max, accel_max)
+        ax_desired = kx * vx_error
     else:
-        thrust = 0.0
-    if y +v_y * dt <= 0:
-        y = 0
-        v_y = 0
-        break
+    # the desired velocities at this point are 0 (rest)
+        target_vy = -v_safe * (y / flare_altitude) # dynamically calculate the target velocity, which eventually goes to zero or rest
+        target_vy = max(target_vy, -v_safe)
+        vy_error = target_vy - v_y
 
-# the direction the engine pushes is a combination of the rocket body's tilt and the engine's gimbal tilt
-    forcex = thrust * np.sin(theta + gimbal_angle)
-    forcey = (thrust * np.cos(theta + gimbal_angle)) - (mass * g)
+        kpy = 2.0
+        kdy = 3.0
+        ay_desired = kpy * (0 - y) + kdy * (target_vy - v_y)
+
+        kpx = 1.0
+        kdx = 2.0
+        ax_desired = kpx * (0 - x) + kdx * (0 - v_x)
+
+    altitude_weight = np.clip(y / 100.0, 0.0,1.0)  # with this variable, the lower to the ground the rocket is, the less horizontal tilt adjustments are made
+    theta_target = np.clip((ax_desired / g) * altitude_weight, -tilt_limit, tilt_limit)  # forces the rocket to tilt in the opposite direction and fight the initial rightwards horizontal movement (v_x0 positive), if the rocket tilts too far to the left and ends up completely changing directions, the negatives cancel, and it will redirect itself.
+    theta_target += -0.1 * theta
+
+    theta_error = theta_target - theta
+# let the gimbal control rotation *only*
+    gimbal_angle = kp * theta_error - kd * omega
+    gimbal_angle = np.clip(gimbal_angle, -0.26, 0.26, out=None)
+
+    thrust = mass * (g + ay_desired)
+    thrust = np.clip(thrust, 0.0, max_thrust)
+
+    # the direction the engine pushes is a combination of the rocket body's tilt and the engine's gimbal tilt
+# let theta control the direction of the thrust
+    forcex = thrust * np.sin(theta)
+    forcey = (thrust * np.cos(theta)) - (mass * g)
 
     accelerationx = forcex / mass
     accelerationy = forcey / mass
@@ -97,6 +101,9 @@ while y > 0.0:
     x_history.append(x)
     y_history.append(y)
     t += dt
+
+    print("VELOCITY LOGS:", "y:", y, "v_y:", v_y, "v_safe:", v_safe)
+    print("ACCELERATION LOGS:", "ay_desired:", ay_desired, "max accel:", accel_max)
 
 print("Rocket Landed/Crashed")
 
